a63d0482844ecfe2bebbdb10b31e9306a3bad754
Mohamed Asidah <moha.as172@gmail.com>
2022-03-16 11:52:14 +0100

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+import java.lang.Character.toChars
+import javax.xml.stream.events.Characters
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+
+    println(Character.toChars(0x1F357))
+}
